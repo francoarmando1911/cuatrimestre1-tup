a774fb38c56d93bb075de566a949aa23236e006b
Franco Armando <francoarmando1911@gmail.com>
2024-11-13 20:58:11 -0300

    13/11

--- src/guia13-funciones/ejercicio2.js
+++ src/guia13-funciones/ejercicio2.js
@@ -4,3 +4,22 @@ persona nos muestre un saludo de buenos días personalizado con ese nombre. Cree
 función para que si es mujer agregue “Sra” y si es hombre “Sr.” delante del nombre
 ingresado.
 */
+
+//ingresar nombre, sexo
+//mostrar saludo de buenos dias personalizado con el nombre de la persona
+// funcion para que si es mujer agregue Sra y si es hombre  Sr
+
+
+function saludo(nombre, sexo){
+    if (sexo.toLowerCase() === 'mujer') {
+        console.log(`Buen día Sra ${nombre}`);
+    } else if (sexo.toLowerCase() === 'hombre') {
+        console.log(`Buen día Sr ${nombre}`);
+    } else {
+        console.log(`Sexo no reconocido. Por favor ingrese "hombre" o "mujer".`);
+    }
+}
+
+let nombre = prompt('Ingrese su nombre: ')
+let sexo = ('Ingrese su sexo: ')
+saludo(nombre, sexo)
