a772172219935d7ccd0c560f16d9abb808b65de9
Franco Armando <francoarmando1911@gmail.com>
2024-11-14 18:26:47 -0300

    ejer5

--- src/ejercicio5.js
+++ src/ejercicio5.js
@@ -5,4 +5,33 @@ y Dolares. El precio de conversión determínelo en un aproximado del valor actu
 internet. Cree una función de conversión de los montos ingresados.
 */
 
-// resultado final en pesos argentinos
+const tasaUruAArg = 0.023 * 1140; // 1 peso uruguayo a pesos argentinos  
+const tasaDolAArg = 1140; // 1 dólar a pesos argentinos  
+  
+function convertirAArg(monto, tipoMoneda) {
+    switch (tipoMoneda) {
+        case 'arg':
+            return monto; 
+        case 'uru':
+            return monto * tasaUruAArg; 
+        case 'dol':
+            return monto * tasaDolAArg; 
+        default:
+            throw new Error("Tipo de moneda no válido");
+    }
+}
+
+function calcularMontoFinal() {
+    const monto = parseFloat(prompt("Ingrese el monto de la compra:"));
+    const tipoMoneda = prompt("Ingrese el tipo de moneda (arg, uru, dol):").toLowerCase();
+
+    try { 
+        const montoFinalEnArg = convertirAArg(monto, tipoMoneda);
+
+        alert(`El monto final en pesos argentinos es: ${montoFinalEnArg.toFixed(2)}`);
+    } catch (error) {
+        alert(error.message); 
+    }
+}
+  
+calcularMontoFinal();
